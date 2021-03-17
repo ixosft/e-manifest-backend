dd7ccff6dad89f39d3b3d44f7686b0e06f6b6560
Oluwafemi Medale <omedale@gmail.com>
2021-03-18 00:13:48 +0100

    improve jwt security

--- lib/utility/json_web_token.rb
+++ lib/utility/json_web_token.rb
@@ -1,19 +1,27 @@
 module Utility
   class JsonWebToken
-    # our secret key to encode our jwt
+    # TODO: move to secret and generate unique AUD
+    ISS = 'e-manifest.com'.freeze
+    AUD = '238d4793-70de-4183-9707-48ed8ecd19d9'.freeze
 
     class << self
       def encode(payload, exp = 48.hours.from_now)
         # set token expiration time
         payload[:exp] = exp.to_i
-
+        payload[:aud] = AUD
+        payload[:iss] = ISS
         # this encodes the user data(payload) with our secret key
-        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+        JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
       end
 
       def decode(token)
         # decodes the token to get user data (payload)
-        body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        body = JWT.decode(
+          token,
+          Rails.application.secrets.secret_key_base,
+          true,
+          { verify_iss: true, iss: ISS, verify_aud: true, aud: AUD, algorithm: 'HS256' }
+        )[0]
         HashWithIndifferentAccess.new body
 
       # raise custom error to be handled by custom handler
