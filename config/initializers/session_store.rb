d7f036d0499a10fc9ffab54c8a5db9ca1439e3ad
Oluwafemi Medale <omedale@gmail.com>
2021-04-10 10:48:10 +0100

    add session_store config

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,9 @@
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, {
+    key: '_application_session',
+    domain: :all,
+    same_site: :none,
+    secure: :true,
+    tld_length: 2
+  }
+end
