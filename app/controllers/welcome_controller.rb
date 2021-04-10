41a31679ac876b9092e4b763da97f6474b516f14
Oluwafemi Medale <omedale@gmail.com>
2021-04-11 00:39:35 +0100

    render json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_request, only: %i[index]
-  def index; end
+  def index
+    render json: { message: 'yes' }
+  end
 end
