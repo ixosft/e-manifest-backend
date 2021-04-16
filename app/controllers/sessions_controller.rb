d124f0717501143d15372b6e5d33c3ec9a8b168e
Oluwafemi Medale <omedale@gmail.com>
2021-04-16 15:11:33 +0100

    more domain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -64,7 +64,7 @@ class SessionsController < ApplicationController
           value: result[:token],
           expires: 24.hour.from_now,
           secure: Rails.env.production?
-        }.merge!(Rails.env.production? ? { same_site: 'None', domain: ['e-manifest.netlify.app'] } : {})
+        }.merge!(Rails.env.production? ? { same_site: 'None', domain: ['.netlify.app', '.herokuapp.com'] } : {})
       )
       render json: V1::UserSerializer.new(result[:user], user_options.auth).serializable_hash
     else
