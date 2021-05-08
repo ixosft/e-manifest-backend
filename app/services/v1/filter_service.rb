26647956caffce0d0e4af8556b713ba3c0746b22
Oluwafemi Medale <omedale@gmail.com>
2021-05-08 15:07:00 +0100

    fix filter

--- app/services/v1/filter_service.rb
+++ app/services/v1/filter_service.rb
@@ -1,25 +1,26 @@
 module V1
   class FilterService
     attr_reader :params, :search, :filter, :resource_table
+    # TODO, verify params
     def initialize(model, params)
-      @params = params
+      @params = params.to_unsafe_h
       @resource_table = model.table_name
-      @search = params[:search] || {}
-      @filter = params[:filter] || {}
+      @search = @params[:search] || {}
+      @filter = @params[:filter] || {}
+      @joins = ''
+      @join_cache = {}
     end
 
     def build_query_params
       search_values, search_query = build_search_query
-      filter_values, filter_joins, filter_columns = build_filter_query
+      filter_values, filter_columns = build_filter_query
 
-      [(search_values + filter_values), (search_query + filter_columns), filter_joins]
+      [(search_values + filter_values), (search_query + (search_query.present? && filter_columns.present? ? ' AND ' : '') + filter_columns), @joins]
     end
 
     def build_filter_query
       values = []
-      joins = ''
       columns = ''
-      join_cache = {}
       if filter.present?
         filter.each do |column_name, value|
           next unless value.present?
@@ -33,31 +34,67 @@ module V1
           end
 
           join_column, join_table, query_column = join_list
-          unless join_cache[join_table]
-            joins += " JOIN #{join_table} on #{resource_table}.#{join_column} = #{join_table}.id "
-          end
+          build_joins(join_table, resource_table, join_column)
           columns += " #{columns.present? ? ' AND ' : ''} #{join_table}.#{query_column} = ? "
-          join_cache[join_table] = true
         end
       end
 
-      [values, joins, columns]
+      [values, columns]
+    end
+
+    def build_special_query(type, table, col, values)
+      if type == 'date'
+        values << Date.parse('YYYY-MM-DD')
+        " DATE(#{table}.#{col}) = DATE(?) "
+      end
+    end
+
+    def build_joins(join_table, resource_table, join_column)
+      unless @join_cache[join_table]
+        @joins += " JOIN #{join_table} on #{resource_table}.#{join_column} = #{join_table}.id "
+        @join_cache[join_table] = true
+      end
     end
 
     def build_search_query
       search_params = [[], '']
-      if search[:query].present? && search[:columns].present?
-        columns = ''
-        query = search[:query]
+      if search.keys.size.positive?
+        column_string = ''
         values = []
-        search[:columns].each_with_index do |column, i|
-          columns += " #{i.positive? ? ' OR ' : ' '}  #{column} ILIKE ('%' || ? || '%')  "
-          values << query
+        table = search.keys.first
+        columns = search[table].keys
+        columns.each_with_index do |column, i|
+          query_data = search[table][column]
+          join_data = search[table][:joins]
+          next unless query_data[:value].present?
+
+          build_joins(join_data[:table], table, join_data[:on]) if join_data.present?
+          all_columns = query_data[:or].present? ? (query_data[:or] + [column]) : [column]
+          all_columns_string = ''
+          all_columns.each_with_index do |col, j|
+            current_column_table = query_data[:table].presence || table
+            query = if query_data[:type].present?
+                      type = query_data[:type]
+                      if type == 'date'
+                        value = Date.parse(query_data[:value]).to_s
+                        " #{current_column_table}.#{col}::date = '#{value}'::date "
+                      elsif type == 'time'
+                        value = Time.parse(query_data[:value]).to_s
+                        " #{current_column_table}.#{col}::time = '#{value}'::time "
+                      end
+                    else
+                      values << query_data[:value]
+                      " #{current_column_table}.#{col} ILIKE ('%' || ? || '%')  "
+                    end
+            all_columns_string = " #{j.positive? ? ' OR ' : ' '}  #{query} "
+          end
+          column_string += " #{i.positive? ? ' AND ' : ' '}  ( #{all_columns_string} )  "
         end
-        search_params = [values, columns]
+        search_params = [values, column_string]
       end
+
       search_params
     end
   end
 end
-Manifest.joins('JOIN motors on motors.id = manifests.motor_id JOIN terminals on terminals.id = manifests.terminal_id')
+# Manifest.joins('JOIN motors on motors.id = manifests.motor_id JOIN terminals on terminals.id = manifests.terminal_id')
